621f0ba806f0c351cbb1b3db6db3510851653724
Casey Weed <me@caseyweed.net>
2015-10-19 16:41:59 -0400

    rename gain.py to sample_gain.py

--- sample_gain.py
+++ sample_gain.py
@@ -12,31 +12,25 @@ Usage:
 
 
 def entropy(l):
-    """Calculate 'parent' entropy."""
     p = Counter(l)
     total = float(len(l))
     return -sum(count/total * math.log(count/total, 2) for count in p.values())
 
-def entropy_column(col, c):
-    pass
-
 def main(args):
     # import data
     data = pandas.read_csv(args['<file>'])
-    print data
     # get class attribute entropy
     class_entropy = entropy(data['class'].tolist())
-    print class_entropy
+    print 'Class entropy:', class_entropy
     # groupby decision attribute (in the future will use ALL columns as a decision attribute)
     decision_entropy = 0
     for name, g in data.groupby(['age']):
-        print 'Group', name
         g_total = g.count().values[0]
         p = Counter(g['class'].tolist())
         r = []
         for v in p.values():
             r.append(-v/g_total * math.log(v/g_total, 2))
-        print g_total, 'over', data.count().values[0], g_total/data.count().values[0] * sum(r)
+        # print g_total, 'over', data.count().values[0], g_total/data.count().values[0] * sum(r)
         decision_entropy += g_total/data.count().values[0] * sum(r)
     print 'Column age:', decision_entropy
     print 'Gain of age:', class_entropy - decision_entropy
